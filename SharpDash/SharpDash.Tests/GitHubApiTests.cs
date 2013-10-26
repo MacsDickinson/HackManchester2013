dad48cbba143e7b472306b3bc172ad0db8a1d10b
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-10-26 16:47:55 +0100

    added simple add/display to make sure raven integration is working

--- SharpDash/SharpDash.Tests/GitHubApiTests.cs
+++ SharpDash/SharpDash.Tests/GitHubApiTests.cs
@@ -11,11 +11,11 @@ namespace SharpDash.Tests
         public void Can_get_events_for_repo()
         {
             // Arrange
-            var client = new RestClient("https://api.github.com")
+            var client = new RestClient("https://api.github.com");
             var request = new RestRequest("/repos/macsdickinson/hackmanchester2013/events", Method.GET);
 
             // Act
-            var response = client.Execute<Event>(request)
+            var response = client.Execute<Event>(request);
 
             // Assert
 
