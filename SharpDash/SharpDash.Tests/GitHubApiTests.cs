f843791d3a1deeb509bc1f215b6ba085336dd890
Matt Ross <matt@tronik.co.uk>
2013-10-26 17:38:32 +0100

    fixed merge conflict
    I'm starting to dislike Git ... merging is much easier in Hg

--- SharpDash/SharpDash.Tests/GitHubApiTests.cs
+++ SharpDash/SharpDash.Tests/GitHubApiTests.cs
@@ -12,7 +12,6 @@ namespace SharpDash.Tests
         public void Can_get_events_for_repo_as_raw_content()
         {
             // Arrange
-<<<<<<< HEAD
             var client = new RestClient{
                                  BaseUrl = "https://api.github.com",
                                  Authenticator = new HttpBasicAuthenticator("autonomatt", "G17hu8")
@@ -22,13 +21,6 @@ namespace SharpDash.Tests
             // Act
             var response = client.Execute<Event>(request);
             //var response = client.Execute(request);
-=======
-            var client = new RestClient("https://api.github.com");
-            var request = new RestRequest("/repos/macsdickinson/hackmanchester2013/events", Method.GET);
-
-            // Act
-            var response = client.Execute<Event>(request);
->>>>>>> dad48cbba143e7b472306b3bc172ad0db8a1d10b
 
             // Assert
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
