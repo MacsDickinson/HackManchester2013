834f6d14e627a2f19ee26d6a480a93dd2557a6c4
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2013-10-26 15:12:48 +0100

    added hello world index page

---
+++ SharpDash/SharpDash/Modules/BaseModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace SharpDash.Modules
+{
+    public class BaseModule : NancyModule
+    {
+        public BaseModule()
+        {
+            Get["/"] = ಠ_ಠ =>
+                {
+                    return View["Index"];
+                };
+        }
+    }
+}
