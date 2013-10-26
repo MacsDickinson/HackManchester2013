8d010d32c0af165e29ec217a08f27b3876e47d5a
Matt Ross <matt@tronik.co.uk>
2013-10-26 16:06:48 +0100

    Added two new projects
    to test GitHub api with RestSharp

---
+++ SharpDash/SharpDash.Tests/GitHubApiTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using SharpDash.Domain.GitHub;
+
+namespace SharpDash.Tests
+{
+    [TestFixture]
+    public class GitHubApiTestsTests
+    {
+        [Test]
+        public void Can_get_events_for_repo()
+        {
+            // Arrange
+            var client = new RestClient("https://api.github.com")
+            var request = new RestRequest("/repos/macsdickinson/hackmanchester2013/events", Method.GET);
+
+            // Act
+            var response = client.Execute<Event>(request)
+
+            // Assert
+
+        }
+    }
+}
